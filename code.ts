a8a1d92128d96f3db2530e57c5dd638d6de1fd99
Noel <noel.dinger@gmail.com>
2020-10-08 16:35:49 +0200

    initial commit

---
+++ code.ts
@@ -0,0 +1,16 @@
+let helloWorld = "Hello World";
+
+<<<<<<< HEAD
+#Weiterer Code ...
+=======
+# dieser code fixt den internet-explorer
+>>>>>>> feature/SNA-1-app-sturzt-ab-im-internet-explorer
+
+
+#dieser code fixt das Firefox ding
+#zweiter versuch
+#versuch 3
+#versuch 4
+
+# ich repariere den button xy
+#chrome bug fix versuch2
